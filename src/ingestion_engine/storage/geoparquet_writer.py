e1024e5852063fe7fbe738c3046be7ebb2cab331
David Ferrer Pérez <davidferrerrugby@gmail.com>
2026-09-03 02:20:06 +0200

    writer refactored

--- src/ingestion_engine/storage/geoparquet_writer.py
+++ src/ingestion_engine/storage/geoparquet_writer.py
@@ -3,15 +3,13 @@ from pyspark.sql import DataFrame
 from ingestion_engine.storage.blob_client import BlobClient
 
 
-def write_df_to_geoparquet(df: DataFrame, blob_client: BlobClient, blob_path: str, overwrite: bool = True) -> None:
-    """Write a Spark DataFrame as GeoParquet directly to Azure Blob Storage."""
-
-    spark_path = blob_client.get_spark_path(blob_path)
-    mode = "overwrite" if overwrite else "errorifexists"
-
-    (
-        df.write
-        .format("geoparquet")
-        .mode(mode)
+def write_df_to_geoparquet(df, blob_client, blob_path):
+    # Sanitize the path for Azure compatibility
+    blob_path_safe = blob_path.replace('+', 'Z').replace(':', '')
+    
+    spark_path = f"wasbs://{blob_client.container_name}@{blob_client.account_name}.blob.core.windows.net/{blob_path_safe}"
+    
+    df.write \
+        .format("geoparquet") \
+        .mode("overwrite") \
         .save(spark_path)
-    )
